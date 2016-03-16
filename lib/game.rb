517461b0187b35a220bd1f06949a8e28f30634ac
adil ali <adiln1993@gmail.com>
2016-03-16 14:25:33 +0000

    challenge 26

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,13 @@
-class Game
+require 'pry'
 
+class Game
+  attr_reader :player1, :player2
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
   end
 
   def attack(player)
-    player.attacked
+    player == player2 ? player2.attacked : player1.attacked
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,14 +1,17 @@
 require 'game'
 
-describe Game do 
+describe Game do
   let(:player2) {double(:player2)}
   let(:player1) {double(:player1)}
   subject(:game) {described_class.new(player1, player2)}
-  
+
   describe ' #attack' do
     it' should call on the player #attacked' do
       expect(player1).to receive(:attacked)
       game.attack(player1)
     end
   end
+
+  it {is_expected.to respond_to(:player1)}
+  it {is_expected.to respond_to(:player2)}
 end
